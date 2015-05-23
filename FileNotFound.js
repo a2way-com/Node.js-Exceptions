89107069560dea16b9b82aa38f10c94a725157e3
Supun Budhajeewa <budhajeewa@gmail.com>
2015-05-23 11:19:54 +0530

    Create "FileNotFound".

---
+++ FileNotFound.js
@@ -0,0 +1,11 @@
+var _ = require('lodash');
+
+var Generic = require('./Generic');
+
+var FileNotFound = function () {
+	var parent = new Generic('FileNotFound');
+
+	_.assign(this, parent);
+};
+
+module.exports = FileNotFound;
